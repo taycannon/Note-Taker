5bbcfc3299866f08eba1376c7c51947d3146a157
taycannon <taycannon2003@gmail.com>
2023-10-21 16:50:17 -0500

    Added api to server as well as put code in HTML file

---
+++ routes/HTMLRoutes.js
@@ -0,0 +1,10 @@
+//HTML Routes
+
+const path = require('path')
+const router = require('express').Router()
+
+router.get('/notes',(req,res)=> {
+    res.sendFile(path.join(__dirname,'../public/notes.html'))
+})
+
+module.exports=router
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 //Server file
 
 const express = require('express');
+const apiRoutes = require('./routes/APIRoutes');
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
