a7f5017708fdb443909d8864edb11d236aebc570
taycannon <taycannon2003@gmail.com>
2023-10-21 17:14:23 -0500

    Added comments to html and server but API had something wrong?

--- routes/APIRoutes.js
+++ routes/APIRoutes.js
@@ -1,28 +1,30 @@
-//API Route
+//Api Routes
+
+const router = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helper/fsUtils');
+const noteID = require('../helper/uuid');
 
 // GET Route
 router.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
-  
-  // POST Route
-  router.post('/notes', (req, res) => {
-    console.log(req.body);
-  
-    const { title, text} = req.body;
-  
-    if (req.body) {
-      const newComment = {
-        title,
-        text,
-        noteID:noteID(),
-      };
-  
-      readAndAppend(newComment, './db/db.json');
-      res.json(`Note added success`);
-    } else {
-      res.error('Error added new note');
-    }
-  });
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-  module.exports = router;
+// POST Route
+router.post('/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text} = req.body;
+
+  if (req.body) {
+    const newComment = {
+      title,
+      text,
+      noteID:noteID(),
+    };
+
+    readAndAppend(newComment, './db/db.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding new note');
+  }
+});
--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,6 +1,10 @@
+//HTML Route 2
+
+//Importing path and router
 const path = require('path')
 const router = require('express').Router()
 
+//Get request that is made to notes
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'../public/notes.html'))
   });
