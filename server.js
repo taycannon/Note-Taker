1a2dbd65fddf6b6d4f0c2ce36d28ac278c16403d
taycannon <taycannon2003@gmail.com>
2023-10-21 16:55:31 -0500

    Having error in routes/HTML because of file path??

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const apiRoutes = require('./routes/APIRoutes');
-
+const htmlRoutes = require('./routes/HTMLRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
-
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
